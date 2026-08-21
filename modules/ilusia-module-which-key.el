b1eb6ecfded9364411e8bf5ff6fee55c530e08ae
Arseniy <arsyhiy32@gmail.com>
2026-08-21 08:19:25 +0300

    update

--- modules/ilusia-module-which-key.el
+++ modules/ilusia-module-which-key.el
@@ -1,7 +1,21 @@
 (use-package which-key
   :straight t
   :hook
-  (after-init . which-key-mode))
+  (after-init . which-key-mode)
+
+  :config
+  (setq which-key-idle-delay 0.5)
+
+  (which-key-add-key-based-replacements
+    "SPC d" "file"
+    "SPC b" "buffer"
+    "SPC c" "search"
+    "SPC p" "project"
+    "SPC g" "git"
+    "SPC l" "language / lsp"
+    "SPC f" "diagnostics"
+    "SPC x" "execute"
+    "SPC w" "window"))
 
 
 (provide 'ilusia-module-which-key)
